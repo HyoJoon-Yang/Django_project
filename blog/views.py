560035aa2f988d50e08f820c29cb6ad927644a35
HyoJoon-Yang <hyojoon.tech@gmail.com>
2023-02-17 09:48:22 +0900

    updaate

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,24 @@ from .models import Post
 from django.views.generic import ListView # 게시판형으로 데이터를 가지고 오는 클래스 
 from django.views.generic.detail import DetailView
 
+class BlogHome(ListView):
+    model = Post
+    ordering = '-pk' 
+    template_name = 'blog/blog_home.html'
+    context_object_name = 'posts'
+
+    # ListView를 상속받으면서 전달받은 get_context_data라는 함수를 오버라이딩합니다
+    def get_context_data(self, **kwargs): # 함수의 파라미터를 개수 안 정해서 k-v 순으로 만들어 보내겠다
+        # 속성명 = 값 -> 파이썬의 namespace에서는 키:밸류 순으로 관리됩니다
+        context = super(BlogHome, self).get_context_data(**kwargs)
+        context['first_post'] = Post.objects.all().last() # first_post라는 이름으로 하나 더 값을 만들어서 전달할게요
+        # 필요한 값들을 ORM으로 뽑아서 가져올 수 있습니다.
+        return context
+
+
 class PostList(ListView):  # post_list 라고 생긴 template과 model을 조합합니다. 
     model = Post
     ordering = '-pk' 
-    # template_name = 'blog/index.html'
 
     # post_list에 전달할 전체 객체명을 바꿔줍니다.
     context_object_name = 'posts'
